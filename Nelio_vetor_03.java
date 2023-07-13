be9688cfbb3310d91c8af4216b7143e89ff70cd0
brunoramos465 <119815913+brunoramos465@users.noreply.github.com>
2023-07-13 17:07:49 -0300

    Add files via upload

---
+++ Nelio_vetor_03.java
@@ -0,0 +1,32 @@
+package URI;
+
+import java.util.Scanner;
+
+public class Nelio_vetor_03{
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int [] a = new int[n];
+		int [] b = new int[n];
+		int [] c = new int[n];
+		
+		for(int i = 0; i < n; i++ ) {
+			a[i] = sc.nextInt();
+		}
+		
+		for(int i = 0; i < n; i++) {
+			b[i] = sc.nextInt();
+		}
+		
+		for(int i = 0; i < n; i++) {
+			c[i] = a[i] + b[i];
+			System.out.print(c[i] + " ");
+
+		}
+		
+	
+		
+		sc.close();
+	}
+}
