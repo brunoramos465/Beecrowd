a6b80d981a2ec290a01b476a3b3c84ffa04804b4
Bruno Ramos <119815913+brunoramos465@users.noreply.github.com>
2023-08-23 19:44:27 -0300

    Add files via upload

---
+++ Bee1149.java
@@ -0,0 +1,28 @@
+package URI;
+
+import java.util.Scanner;
+
+public class Bee1149{
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int n = sc.nextInt();
+		int soma = 0;
+		
+		
+		
+		while (n <= 0) {
+			n = sc.nextInt();
+		}
+		
+		for (int i = 1; i <= n;i++) {
+			soma += a;
+			a++;
+		}
+		
+		System.out.println(soma);
+		
+		sc.close();
+	}
+}
